c4a32e9bdb3e3f3a9dc0c06335d7ab3ddcbdfa50
tungnhann <23521077@gm.uit.edu.vn>
2026-02-28 15:11:52 +0700

    fix

--- src/main/java/com/example/shopnow/product/seller/zz.java
+++ src/main/java/com/example/shopnow/product/seller/zz.java
@@ -2,6 +2,6 @@ package com.example.shopnow.product.seller;
 
 public class zz {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worldd");
     }
 }
