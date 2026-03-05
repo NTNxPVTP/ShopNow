c6bcf7baed330c5f81d2a6c5eb1496177a76e145
tungnhann <23521077@gm.uit.edu.vn>
2026-03-05 21:26:19 +0700

    doneViewProductDetails

--- src/main/java/com/example/shopnow/product/ProductRepository.java
+++ src/main/java/com/example/shopnow/product/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.example.shopnow.product;
 
-public interface ProductRepository implements JpaRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer > {
+    public Product findByIdProduct(Integer idProduct);
 }
