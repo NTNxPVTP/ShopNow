1db006a9116d7bf4f9113409b18d7db159f34245
PhamPhien-gif <toilaphien2302@gmail.com>
2026-03-07 09:34:07 +0700

    feature: createProduct

--- server/src/main/java/com/example/shopnow/product/ProductService.java
+++ server/src/main/java/com/example/shopnow/product/ProductService.java
@@ -4,10 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional( readOnly= true, rollbackFor = Exception.class)
 public class ProductService {
     @Autowired
     ProductRepository productRepository;
     public Product viewDetailsOfProduct(Integer id) {
         return productRepository.findByIdProduct(id);
+    }
+
+    public ProductDetail createProduct(CreateProductRequest request){
+        Product product = ProductMapper.fromRequestToProduct(request);
+        product = productRepository.save(product);
+        ProductDetail detail = ProductMapper.toDetail(product);
+        return detail;
     }
 }
