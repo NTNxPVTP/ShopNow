33d51ebc6bbaa16c7e2afb54427e3b1266501ed5
PhamPhien-gif <toilaphien2302@gmail.com>
2026-03-16 10:55:44 +0700

    Unit test

--- server/src/test/java/com/example/shopnow/ArchitectureTest.java
+++ server/src/test/java/com/example/shopnow/ArchitectureTest.java
@@ -4,8 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 
 public class ArchitectureTest {
+    ApplicationModules modules = ApplicationModules.of(ShopnowApplication.class);
+
     @Test
     void verifyBoundary() {
-        ApplicationModules.of(ShopnowApplication.class).verify();
+        modules.verify();
     }
+
+    // @Test
+    // void internal_class_should_not_be_access_from_outside() {
+    //     JavaClasses importedClass = new ClassFileImporter().importPackages("com.example.shopnow");
+
+    //     ArchRule encapsulationRule = classes()
+    //             .that().resideInAPackage("..product..")
+    //             .should().onlyBeAccessed().byAnyPackage("..product..");
+
+    //     encapsulationRule.check(importedClass);
+    // }
+
 }
