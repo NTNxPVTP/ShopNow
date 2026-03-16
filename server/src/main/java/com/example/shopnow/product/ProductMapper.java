a4377312fe339f1e8a4ecff81c2f6d5d1a5e918b
PhamPhien-gif <toilaphien2302@gmail.com>
2026-03-16 13:16:15 +0700

    feature: createProduct

--- server/src/main/java/com/example/shopnow/product/ProductMapper.java
+++ server/src/main/java/com/example/shopnow/product/ProductMapper.java
@@ -3,6 +3,7 @@ package com.example.shopnow.product;
 import org.springframework.stereotype.Component;
 
 import com.example.shopnow.product.models.Product;
+import com.example.shopnow.product.rest.dto.CreateProductRequest;
 import com.example.shopnow.product.rest.dto.ProductDetailResponse;
 
 @Component
@@ -11,9 +12,18 @@ class ProductMapper {
         return new ProductDetailResponse(
             product.getId(),
             product.getName(),
-            product.getPicture_url(),
+            product.getPictureUrl(),
             product.getQuantity(),
             product.getPrice(),
             product.getStatus());
+    }
+    public Product fromRequestToProduct(CreateProductRequest request){
+        System.out.println(request);
+        return Product.builder()
+                    .name(request.name())
+                    .pictureUrl(request.pictureUrl())
+                    .price(request.price())
+                    .quantity(request.quantity())
+                    .build();
     }
 }
