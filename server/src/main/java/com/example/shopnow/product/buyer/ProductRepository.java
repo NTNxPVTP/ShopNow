3af9f0d0b5b3e6ae468aed9b49d033df1bf668f7
tungnhann <23521077@gm.uit.edu.vn>
2026-03-16 11:27:10 +0700

    none

--- server/src/main/java/com/example/shopnow/product/buyer/ProductRepository.java
+++ server/src/main/java/com/example/shopnow/product/buyer/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.example.shopnow.product.buyer;
 
-import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.shopnow.product.models.Product;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.shopnow.product.models.Product;
+@Repository
 interface ProductRepository extends JpaRepository<Product, UUID > {
-    Optional<Product> findById(UUID idProduct);
+    
 }
     
--- server/src/main/java/com/example/shopnow/product/seller/domain/ProductSellerRepository.java
+++ server/src/main/java/com/example/shopnow/product/seller/domain/ProductSellerRepository.java
@@ -1,12 +1,12 @@
 package com.example.shopnow.product.seller.domain;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.shopnow.product.models.Product;
-
+@Repository
 interface ProductSellerRepository extends JpaRepository<Product, UUID>{
-    Optional<Product> findById(UUID id);
+    
 }
