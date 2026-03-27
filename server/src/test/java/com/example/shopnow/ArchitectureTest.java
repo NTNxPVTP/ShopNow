a425a7fea7af19b62e79a8b5f9b367b3e7c2d703
tungnhann <23521077@gm.uit.edu.vn>
2026-03-27 10:02:07 +0700

    Refresh gitignore rules

--- server/src/test/java/com/example/shopnow/ArchitectureTest.java
+++ server/src/test/java/com/example/shopnow/ArchitectureTest.java
@@ -1,14 +1,14 @@
-// package com.example.shopnow;
+package com.example.shopnow;
 
-// import org.junit.jupiter.api.Test;
-// // import org.springframework.modulith.core.ApplicationModules;
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
 
-// public class ArchitectureTest {
-// ApplicationModules modules = ApplicationModules.of(ShopnowApplication.class);
+public class ArchitectureTest {
+ApplicationModules modules = ApplicationModules.of(ShopnowApplication.class);
 
-// @Test
-// void verifyBoundary() {
-// modules.verify();
-// }
+@Test
+void verifyBoundary() {
+modules.verify();
+}
 
-// }
+}
